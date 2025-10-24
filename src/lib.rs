560ead4372919cca94df4b7e7430a7a1547ae34c
Sebastien Klasa <klasa.sebastien@gmail.com>
2025-10-24 20:54:25 +0000

    feat: add render function and shared state

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,21 @@
+use console_error_panic_hook;
+use debug_cell::RefCell;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use web_sys::WebGl2RenderingContext;
 
 mod drawing;
 mod webgl;
 
+struct App {
+    gl: WebGl2RenderingContext,
+    board: Board,
+}
+
 #[wasm_bindgen(start)]
 fn start() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+
     let canvas = web_sys::window()
         .ok_or("cannot get window")?
         .document()
@@ -14,79 +24,86 @@ fn start() -> Result<(), JsValue> {
         .ok_or("cannot get canvas")?
         .dyn_into::<web_sys::HtmlCanvasElement>()?;
 
-    let gl = canvas
-        .get_context("webgl2")?
-        .ok_or("cannot get webgl2 context")?
-        .dyn_into::<WebGl2RenderingContext>()?;
+    let state = Rc::new(RefCell::new(App {
+        gl: canvas
+            .get_context("webgl2")?
+            .ok_or("cannot get webgl2 context")?
+            .dyn_into::<WebGl2RenderingContext>()?,
+        #[rustfmt::skip]
+        board: Board { cells: [
+            false, false, false, false, false, false, false, false, false, false,
+            false, false, false, false,  true, false, false, false, false, false,
+            false, false, false, false, false,  true, false, false, false, false,
+            false, false, false, false, false, false, false, false, false, false,
+            false, false, false, false, false, false, false, false, false, false,
+            false,  true,  true, false, false, false, false,  true, false, false,
+            false, false, false, false, false, false, false, false, false, false,
+            false, false, false, false, false, false, false, false, false, false,
+            false, false, false, false, false, false, false, false, false, false,
+            false, false, false, false, false, false, false, false, false, false,
+        ]},
+    }));
 
-    // https://webglfundamentals.org/webgl/lessons/webgl-fundamentals.html
-    // the vertex shader computes vertex positions
-    // webgl uses its output to rasterize primitives (point, line, triangle)
-    let vertex_shader = webgl::compile_shader(
-        &gl,
-        WebGl2RenderingContext::VERTEX_SHADER,
-        r#"
-        // receives data from the buffer
-        attribute vec4 position;
-        void main() {
-            // gl_Position is the output of the shader
-            gl_Position = position;
-        }
-    "#,
-    )?;
+    {
+        // use gl in a scope to release the state's RefCell
+        let ref gl = &state.borrow().gl;
 
-    // the fragment shader computes the color of each pixel of the drawn primitive
-    let fragment_shader = webgl::compile_shader(
-        &gl,
-        WebGl2RenderingContext::FRAGMENT_SHADER,
-        r#"
-        // choose a precision for the fragment shader (mediump)
-        precision mediump float;
-        void main() {
-            // gl_FragColor is the output of the shader
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-        }
-    "#,
-    )?;
+        // https://webglfundamentals.org/webgl/lessons/webgl-fundamentals.html
+        // the vertex shader computes vertex positions
+        // webgl uses its output to rasterize primitives (point, line, triangle)
+        let vertex_shader = webgl::compile_shader(
+            &gl,
+            WebGl2RenderingContext::VERTEX_SHADER,
+            r#"
+                // receives data from the buffer
+                attribute vec4 position;
+                void main() {
+                    // gl_Position is the output of the shader
+                    gl_Position = position;
+                }
+            "#,
+        )?;
 
-    // providing data to the gpu:
-    // - buffers contains data that attributes extract
-    // - uniforms are global variables set before executing the shader
-    // - textures
-    // - varying are used by the vertex shader to pass data to the fragment shader
+        // the fragment shader computes the color of each pixel of the drawn primitive
+        let fragment_shader = webgl::compile_shader(
+            &gl,
+            WebGl2RenderingContext::FRAGMENT_SHADER,
+            r#"
+                // choose a precision for the fragment shader (mediump)
+                precision mediump float;
+                void main() {
+                    // gl_FragColor is the output of the shader
+                    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+                }
+            "#,
+        )?;
 
-    // --- out of loop ---
+        // providing data to the gpu:
+        // - buffers contains data that attributes extract
+        // - uniforms are global variables set before executing the shader
+        // - textures
+        // - varying are used by the vertex shader to pass data to the fragment shader
 
-    let program = webgl::link_program(&gl, &vertex_shader, &fragment_shader)?;
-    gl.use_program(Some(&program));
+        let program = webgl::link_program(&gl, &vertex_shader, &fragment_shader)?;
+        gl.use_program(Some(&program));
 
-    let buffer = gl.create_buffer().ok_or("cannot create buffer")?;
-    gl.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
+        let buffer = gl.create_buffer().ok_or("cannot create buffer")?;
+        gl.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
 
-    let position = gl.get_attrib_location(&program, "position") as u32;
-    gl.vertex_attrib_pointer_with_i32(position, 2, WebGl2RenderingContext::FLOAT, false, 0, 0);
-    gl.enable_vertex_attrib_array(position);
+        let position = gl.get_attrib_location(&program, "position") as u32;
+        gl.vertex_attrib_pointer_with_i32(position, 2, WebGl2RenderingContext::FLOAT, false, 0, 0);
+        gl.enable_vertex_attrib_array(position);
+    }
 
-    #[rustfmt::skip]
-    let board = Board { cells: [
-        false, false, false, false, false, false, false, false, false, false,
-        false, false, false, false,  true, false, false, false, false, false,
-        false, false, false, false, false,  true, false, false, false, false,
-        false, false, false, false, false, false, false, false, false, false,
-        false, false, false, false, false, false, false, false, false, false,
-        false,  true,  true, false, false, false, false,  true, false, false,
-        false, false, false, false, false, false, false, false, false, false,
-        false, false, false, false, false, false, false, false, false, false,
-        false, false, false, false, false, false, false, false, false, false,
-        false, false, false, false, false, false, false, false, false, false,
-    ]};
-
-    // --- rendering loop ---
-
-    drawing::clear(&gl);
-    board.draw(&gl);
+    render(0, &mut *state.borrow_mut());
 
     Ok(())
+}
+
+fn render(time_ms: u64, app: &mut App) {
+    app.board.advance();
+    drawing::clear(&app.gl);
+    app.board.draw(&app.gl);
 }
 
 struct Board {
@@ -97,7 +114,7 @@ impl Board {
     const WIDTH: usize = 10;
     const HEIGHT: usize = 10;
 
-    fn draw(self, gl: &WebGl2RenderingContext) {
+    fn draw(&self, gl: &WebGl2RenderingContext) {
         let grid_dimensions = drawing::GridDimensions {
             x: -0.5,
             y: -0.5,
