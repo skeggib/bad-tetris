601ed63e479999a12d5660eff13c14d7d8177e73
Sebastien Klasa <klasa.sebastien@gmail.com>
2025-11-21 23:01:06 +0000

    refactor: move board tests to separate files

---
+++ src/board/block_physics_tests.rs
@@ -0,0 +1,247 @@
+use super::*;
+
+#[test]
+fn advance_moves_block_one_cell_down() {
+    #[rustfmt::skip]
+    let mut board = Board::<5, 5>::new([
+        X, o, o, o, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+    ]);
+    #[rustfmt::skip]
+    let expected_board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        X, o, o, o, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+    ]);
+    board.advance();
+    assert_eq!(board, expected_board);
+}
+
+#[test]
+fn advance_falling_block_stops_on_bottom() {
+    #[rustfmt::skip]
+    let mut board = Board::<5, 5>::new([
+        X, o, o, o, o,
+        o, o, o, o, o,
+        o, X, o, o, o,
+        o, o, o, o, X,
+        o, o, o, o, o,
+    ]);
+    #[rustfmt::skip]
+    let expected_board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+        X, X, o, o, X,
+    ]);
+    for _ in 0..4 {
+        board.advance();
+    }
+    assert_eq!(board, expected_board);
+}
+
+#[test]
+fn advance_falling_block_stops_on_other_block() {
+    #[rustfmt::skip]
+    let mut board = Board::<5, 5>::new([
+        X, o, X, o, X,
+        o, o, X, o, X,
+        o, o, o, o, o,
+        o, o, o, o, X,
+        o, o, o, o, o,
+    ]);
+    #[rustfmt::skip]
+    let expected_board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        o, o, o, o, o,
+        o, o, o, o, X,
+        o, o, X, o, X,
+        X, o, X, o, X,
+    ]);
+    for _ in 0..4 {
+        board.advance();
+    }
+    assert_eq!(board, expected_board);
+}
+
+#[test]
+fn left_moves_blocks_to_the_left() {
+    #[rustfmt::skip]
+    let mut board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        o, X, o, o, o,
+        o, o, o, X, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+    ]);
+    #[rustfmt::skip]
+    let expected_board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        X, o, o, o, o,
+        o, o, X, o, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+    ]);
+    board.left();
+    assert_eq!(board, expected_board);
+}
+
+#[test]
+fn left_stops_at_walls() {
+    #[rustfmt::skip]
+    let mut board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        X, o, o, o, o,
+        o, o, o, X, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+    ]);
+    #[rustfmt::skip]
+    let expected_board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        X, o, o, o, o,
+        o, o, X, o, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+    ]);
+    board.left();
+    assert_eq!(board, expected_board);
+}
+
+#[test]
+fn left_stops_at_other_blocks() {
+    #[rustfmt::skip]
+    let mut board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        X, X, o, o, o,
+        o, o, o, X, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+    ]);
+    #[rustfmt::skip]
+    let expected_board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        X, X, o, o, o,
+        o, o, X, o, o,
+        o, o, o, o, o,
+        o, o, o, o, o,
+    ]);
+    board.left();
+    assert_eq!(board, expected_board);
+}
+
+#[test]
+fn left_only_moves_falling_blocks() {
+    #[rustfmt::skip]
+    let mut board = Board::<5, 5>::new([
+        o, o, o, X, o, 
+        o, o, o, X, o, 
+        o, X, o, o, o, 
+        o, o, o, o, X, 
+        o, X, o, o, X,
+    ]);
+    #[rustfmt::skip]
+    let expected_board = Board::<5, 5>::new([
+        o, o, X, o, o, 
+        o, o, X, o, o, 
+        X, o, o, o, o, 
+        o, o, o, o, X, 
+        o, X, o, o, X, 
+    ]);
+    board.left();
+    assert_eq!(board, expected_board);
+}
+
+#[test]
+fn right_moves_blocks_to_the_right() {
+    #[rustfmt::skip]
+    let mut board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        X, o, o, o, o,
+        o, o, o, o, o,
+        o, o, X, o, o,
+        o, o, o, o, o,
+    ]);
+    #[rustfmt::skip]
+    let expected_board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        o, X, o, o, o,
+        o, o, o, o, o,
+        o, o, o, X, o,
+        o, o, o, o, o,
+    ]);
+    board.right();
+    assert_eq!(board, expected_board);
+}
+
+#[test]
+fn right_stops_at_walls() {
+    #[rustfmt::skip]
+    let mut board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        X, o, o, o, o,
+        o, o, o, o, o,
+        o, o, o, o, X,
+        o, o, o, o, o,
+    ]);
+    #[rustfmt::skip]
+    let expected_board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        o, X, o, o, o,
+        o, o, o, o, o,
+        o, o, o, o, X,
+        o, o, o, o, o,
+    ]);
+    board.right();
+    assert_eq!(board, expected_board);
+}
+
+#[test]
+fn right_stops_at_other_blocks() {
+    #[rustfmt::skip]
+    let mut board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        X, o, o, o, o,
+        o, o, o, o, o,
+        o, o, o, X, X,
+        o, o, o, o, o,
+    ]);
+    #[rustfmt::skip]
+    let expected_board = Board::<5, 5>::new([
+        o, o, o, o, o,
+        o, X, o, o, o,
+        o, o, o, o, o,
+        o, o, o, X, X,
+        o, o, o, o, o,
+    ]);
+    board.right();
+    assert_eq!(board, expected_board);
+}
+
+#[test]
+fn right_only_moves_falling_blocks() {
+    #[rustfmt::skip]
+    let mut board = Board::<5, 5>::new([
+        o, o, X, o, o, 
+        o, o, X, o, o, 
+        X, o, o, o, o, 
+        o, o, o, o, X, 
+        X, o, o, o, X, 
+    ]);
+    #[rustfmt::skip]
+    let expected_board = Board::<5, 5>::new([
+        o, o, o, X, o, 
+        o, o, o, X, o, 
+        o, X, o, o, o, 
+        o, o, o, o, X, 
+        X, o, o, o, X, 
+    ]);
+    board.right();
+    assert_eq!(board, expected_board);
+}
---
+++ src/board/tetromino_physics_tests.rs
@@ -0,0 +1,424 @@
+use super::*;
+
+#[test]
+fn tetromino_spawns_when_all_blocks_have_fallen() {
+    // given falling blocks
+    #[rustfmt::skip]
+    let mut board = Board::<7, 7>::new([
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+        o, o, X, o, o, o, o,
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+    ]);
+
+    // when all blocks have fallen
+    board.advance();
+    board.advance();
+
+    // and when the board is updated
+    board.advance();
+
+    // then a new tetromino spawns
+    #[rustfmt::skip]
+    let expected = Board::<7, 7>::new([
+        o, o, o, X, o, o, o,
+        o, o, X, X, X, o, o,
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+        o, o, X, o, o, o, o,
+    ]);
+    assert_eq!(board, expected);
+}
+
+#[test]
+fn advance_moves_tetromino_one_cell_down() {
+    // given a newly spawn tetromino
+    let mut board = Board::<7, 7>::new([o; 7 * 7]);
+    board.advance();
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, X, o, o, o,
+            o, o, X, X, X, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+
+    // when the game advances
+    board.advance();
+
+    // then the tetromino moves one cell down
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, X, o, o, o,
+            o, o, X, X, X, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+}
+
+#[test]
+fn left_stops_tetromino_at_walls() {
+    // given a tetromino adjacent to the left wall
+    let mut board = Board::<7, 7>::new([o; 7 * 7]);
+    board.advance();
+    board.advance();
+    board.advance();
+    board.advance();
+    board.left();
+    board.left();
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, X, o, o, o, o, o,
+            X, X, X, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+
+    // when the player presses 'left'
+    board.left();
+
+    // then the tetromino does not move
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, X, o, o, o, o, o,
+            X, X, X, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+}
+
+#[test]
+fn right_stops_tetromino_at_walls() {
+    // given a tetromino adjacent to the right wall
+    let mut board = Board::<7, 7>::new([o; 7 * 7]);
+    board.advance();
+    board.advance();
+    board.advance();
+    board.advance();
+    board.right();
+    board.right();
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+                           o, o, o, o, o, o, o,
+                           o, o, o, o, o, o, o,
+                           o, o, o, o, o, o, o,
+                           o, o, o, o, o, X, o,
+                           o, o, o, o, X, X, X,
+                           o, o, o, o, o, o, o,
+                           o, o, o, o, o, o, o,
+        ])
+    );
+
+    // when the player presses 'right'
+    board.right();
+
+    // then the tetromino does not move
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+                           o, o, o, o, o, o, o,
+                           o, o, o, o, o, o, o,
+                           o, o, o, o, o, o, o,
+                           o, o, o, o, o, X, o,
+                           o, o, o, o, X, X, X,
+                           o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+}
+
+#[test]
+fn falling_tetromino_dismantles_at_bottom() {
+    // given a tetromino adjacent to the bottom wall
+    let mut board = Board::<7, 7>::new([o; 7 * 7]);
+    board.advance();
+    board.advance();
+    board.rotate();
+    board.advance();
+    board.advance();
+    board.advance();
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, X, o, o, o,
+            o, o, o, X, X, o, o,
+            o, o, o, X, o, o, o,
+        ])
+    );
+
+    // when the game advances
+    board.advance();
+
+    // then the tetromino dismantles and individual blocks continue falling
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, X, o, o, o,
+            o, o, o, X, o, o, o,
+            o, o, o, X, X, o, o,
+        ])
+    );
+}
+
+#[test]
+fn rotating_a_t() {
+    #[rustfmt::skip]
+    let mut board = Board::<7, 7>::new([
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+        o, o, o, o, o, o, o,
+    ]);
+    board.advance();
+    board.advance();
+
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, X, o, o, o,
+            o, o, X, X, X, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+
+    board.rotate();
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, X, o, o, o,
+            o, o, o, X, X, o, o,
+            o, o, o, X, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+
+    board.rotate();
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, X, X, X, o, o,
+            o, o, o, X, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+
+    board.rotate();
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, X, o, o, o,
+            o, o, X, X, o, o, o,
+            o, o, o, X, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+
+    board.rotate();
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, X, o, o, o,
+            o, o, X, X, X, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+}
+
+#[test]
+fn rotating_a_tetromino_adjacent_to_left_wall_making_it_overflow_the_grid() {
+    // given a tetromino adjacent to a wall
+    let mut board = Board::<7, 7>::new([false; 7 * 7]);
+    board.advance();
+    board.advance();
+    board.rotate();
+    board.left();
+    board.left();
+    board.left();
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            X, o, o, o, o, o, o,
+            X, X, o, o, o, o, o,
+            X, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+
+    // when a rotation makes it overflow the grid
+    board.rotate();
+
+    // then it is moved back to the limits of the grid
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            X, X, X, o, o, o, o,
+            o, X, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+}
+
+#[test]
+fn rotating_a_tetromino_adjacent_to_right_wall_making_it_overflow_the_grid() {
+    // given a tetromino adjacent to a wall
+    let mut board = Board::<7, 7>::new([false; 7 * 7]);
+    board.advance();
+    board.advance();
+    board.rotate();
+    board.rotate();
+    board.rotate();
+    board.right();
+    board.right();
+    board.right();
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, X,
+            o, o, o, o, o, X, X,
+            o, o, o, o, o, o, X,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+
+    // when a rotation makes it overflow the grid
+    board.rotate();
+
+    // then it is moved back to the limits of the grid
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, X, o,
+            o, o, o, o, X, X, X,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+        ])
+    );
+}
+
+#[test]
+fn tetromino_cannot_be_rotated_on_the_ground() {
+    // given a tetromino that just moved to the ground
+    let mut board = Board::<7, 7>::new([false; 7 * 7]);
+    board.advance();
+    board.advance();
+    board.advance();
+    board.advance();
+    board.advance();
+    board.advance();
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, X, o, o, o,
+            o, o, X, X, X, o, o,
+        ])
+    );
+
+    // when the tetromino is rotated
+    board.rotate();
+
+    // then it does nothing because it has already been dismantled
+    assert_eq!(
+        board,
+        #[rustfmt::skip]
+        Board::<7, 7>::new([
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, o, o, o, o,
+            o, o, o, X, o, o, o,
+            o, o, X, X, X, o, o,
+        ])
+    );
+}
